64c4b7a747aae7a38f6cef636f3149148eedcab5
E-gap <106596181+E-gap@users.noreply.github.com>
2022-10-10 16:04:22 +0300

    task 2 complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 console.log(galleryItems);
+
+
+
+const gallery = document.querySelector('.gallery');
+
+
+
+const newGallery = galleryItems.map(({ preview, original, description}) => 
+    `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} title="подпись"/>
+</a>`)
+    .join("");
+
+gallery.insertAdjacentHTML('afterbegin', newGallery);
+
+
+const onGalleryClickOpen = (event) => {
+    event.preventDefault();
+    if (event.target.classList.contains("gallery__image")) {
+        const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
+        
+        
+    }
+                    
+}      
+    gallery.addEventListener('click', onGalleryClickOpen); 
